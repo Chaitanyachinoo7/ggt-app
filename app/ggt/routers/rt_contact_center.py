72f5699ae2a9cdc1cf95b404c05f6c53d374c8a4
Samar Swami <samarswami@Samars-MacBook-Air.local>
2020-08-20 19:39:34 -0400

    SMS and Email APIs

--- app/ggt/routers/rt_contact_center.py
+++ app/ggt/routers/rt_contact_center.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Request, Response, status
 from ggt.lib.sms import send_sms
+from ggt.lib.email import send_email, render_template
 from ggt.models.data_models.data_types import (
-    CCSendSMSRequest
+    CCSendSMSRequest,
+    CCSendEmailRequest,
+    CCSendNotiRequest
 )
 from ggt.lib.utils import (
     get_config_val,
@@ -24,3 +27,54 @@ async def api_cc_send_sms(CCSendSMSRequest: CCSendSMSRequest):
         return {"status": "success"}
     except Exception as err:
         print(err)
+
+
+@router.post("/sendemail")
+async def api_cc_send_email(CCSendEmailRequest: CCSendEmailRequest):
+    try:
+        email = formatted_email_message(
+            CCSendEmailRequest.first_name, CCSendEmailRequest.token, CCSendEmailRequest.to_email)
+        send_email(email["from_email"], email["from_name"],
+                   email["to_email"], email["subject"], email["html_content"])
+        return {"status": "success"}
+    except Exception as err:
+        print(err)
+
+
+@router.post("/sms_email_notify")
+async def api_cc_send_sms_email(CCSendNotiRequest: CCSendNotiRequest):
+    try:
+        email = formatted_email_message(
+            CCSendNotiRequest.first_name, CCSendNotiRequest.token, CCSendNotiRequest.to_email)
+        send_email(email["from_email"], email["from_name"],
+                   email["to_email"], email["subject"], email["html_content"])
+        send_sms(CCSendNotiRequest.to_number, formatted_sms_message(
+            CCSendNotiRequest.first_name, CCSendNotiRequest.token))
+        return {"status": "success"}
+    except Exception as err:
+        print(err)
+
+
+def formatted_email_message(first_name, token, to_email):
+    base_url = get_config_val('base_url')
+    from_email = get_config_val('notifications.from_email')
+    from_name = get_config_val('notifications.from_name')
+    subject = get_config_val('notifications.result_subject')
+
+    template_vars = {
+        "first_name": first_name,
+        "result_link": "{}/r/{}".format(base_url, token)
+    }
+
+    template_name = get_config_val('notifications.result_template')
+    html_content = render_template(template_name, **template_vars)
+
+    email_message = {
+        'from_email': from_email,
+        'from_name': from_name,
+        'to_email': to_email,
+        'subject': subject,
+        'html_content': html_content
+    }
+
+    return email_message
