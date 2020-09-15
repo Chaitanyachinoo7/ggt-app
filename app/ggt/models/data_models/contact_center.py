ad14bc6c01ed5aa0ab3a950b4d66bb41077e3f8b
Samar Swami <samarswami@Samars-MacBook-Air.local>
2020-09-14 20:10:17 -0400

    outbound call logs

--- app/ggt/models/data_models/contact_center.py
+++ app/ggt/models/data_models/contact_center.py
@@ -11,22 +11,22 @@ from ggt.lib.adapters.mysql_adapter import (
 )
 
 
-def update_outbound_call_status(test_id,
-                                first_name,
-                                test_date,
-                                dob,
-                                token,
-                                to_email,
-                                to_number,
-                                test_result,
-                                call_status):
+def add_outbound_call_status(test_id,
+                             first_name,
+                             test_date,
+                             dob,
+                             token,
+                             to_email,
+                             to_number,
+                             test_result,
+                             call_status, call_initiated_dt):
     try:
-        delete_status = __delete_earlier_status(test_id)
+        # delete_status = __delete_earlier_status(test_id)
         sql = """
-            INSERT INTO outbound_results_status 
-                (test_id,first_name,test_date,dob,token, to_email,to_number,test_result,call_status)
+            INSERT INTO outbound_results_logs
+                (test_id,first_name,test_date,dob,token, to_email,to_number,test_result,call_status, call_initiated_dt)
             
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             """
 
         val = (test_id,
@@ -37,8 +37,7 @@ def update_outbound_call_status(test_id,
                to_email,
                to_number,
                test_result,
-               call_status)
-
+               call_status, call_initiated_dt)
         return exec_insert(sql, val)
 
     except Exception as err:
@@ -50,16 +49,29 @@ def update_outbound_call_status(test_id,
                                         to_email,
                                         to_number,
                                         test_result,
-                                        call_status), locals=locals(),
+                                        call_status, call_initiated_dt), locals=locals(),
+                    function='add_outbound_call_status', error=err)
+        return None
+
+
+def update_outbound_call_status(test_id, call_status, datetime_field, datetime):
+    try:
+        sql = "UPDATE outbound_results_logs SET " + datetime_field + " = '" + \
+            str(datetime)+"', call_status = '" + \
+            call_status+"' WHERE test_id = "+test_id
+        return exec_update(sql, )
+    except Exception as err:
+        log_generic(type="error", data=(test_id, datetime_field, datetime),
                     function='update_outbound_call_status', error=err)
         return None
 
+
 def __delete_earlier_status(test_id):
-        sql = """
+    sql = """
         DELETE FROM 
             outbound_results_status
         WHERE
             test_id = %s
         """
-        vals = (test_id,)
-        return exec_delete(sql, vals)
+    vals = (test_id,)
+    return exec_delete(sql, vals)
