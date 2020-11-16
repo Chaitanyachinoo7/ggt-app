432d90e88b0e1525b3466dfe16728d9fc92a588f
Visitha Wijesinghe <visitha@gogettested.com>
2020-11-16 12:01:17 +0000

    Auto-generate site-code when creating a location (GGT-87)

--- app/ggt/models/data_models/data_types.py
+++ app/ggt/models/data_models/data_types.py
@@ -531,26 +531,26 @@ class GgtLocation(BaseModel):
 
 
 class GgtDbLocation(BaseModel):
-    site_code: str = None
+    site_code: str = 'GGT'
     group_code: Optional[str] = None
     name: str = None
     addr1: str = None
     addr2: Optional[str] = None
     addr3: Optional[str] = None
     city: str = None
-    st: str = None
+    st: str = 'TX'
     zip: str = None
     lat: Optional[float] = None
     lng: Optional[float] = None
-    time_zone: str = None
-    time_zone_offset: str = None
-    test_type_offered: str = None
-    status: str = None
-    type: str = None
-    billing_type: str = None
-    collect_insurance_info: bool = None
-    allow_insurance_skip: bool = None
-    collect_upfront_payment: bool = None
+    time_zone: str = 'CST'
+    time_zone_offset: str = '-05:00'
+    test_type_offered: str = 'oral'
+    status: str = 'enabled'
+    type: str = 'drive_thru'
+    billing_type: str = 'insurance'
+    collect_insurance_info: bool = 0
+    allow_insurance_skip: bool = 1
+    collect_upfront_payment: bool = 0
     image_thumbnail: Optional[str] = None
 
 
