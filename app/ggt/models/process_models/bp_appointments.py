7ae83ddf99d6101fbd31823537d82636553dd1cf
bluedevilx <sureshd@gmail.com>
2020-11-20 13:06:23 -0500

    removed output files

--- app/ggt/models/process_models/bp_appointments.py
+++ app/ggt/models/process_models/bp_appointments.py
@@ -63,14 +63,7 @@ def bp_get_appointment_info(appointment_id, dob):
 def bp_appointment_update(appointment_id: int, action: str, workstation_id: int):
     try:
         appointment = get_appointment(appointment_id)
-        if action == 'checkin' or action == 'check_in':
-            update_appointment_with_checkin(appointment.id)
-        elif action == 'start_test':
-            __appointment_begin_test(appointment.id, workstation_id)
-        elif action == 'scan_vial':
-            update_appointment_with_scan_vial(appointment_id)
-        elif action == 'end_test':
-            update_appointment_with_test_completed(appointment.id)
+            
         if action == c.APPOINTMENT_ACTION_CHECK_IN:
             update_appointment_with_checkin(appointment_id)
 
@@ -84,7 +77,7 @@ def bp_appointment_update(appointment_id: int, action: str, workstation_id: int)
             update_appointment_with_test_completed(appointment_id)
             __send_test_complete_sms(appointment)
             
-        elif action == 'reprint':
+        elif action == c.APPOINTMENT_ACTION_REPRINT:
             __appointment_reprint_label(appointment_id, workstation_id)
 
         #TODO: This allows the start_test to be invoked twice (print the label twice). And every other action only to be invoked once.
