60fb0fd4c09eb9a6dad7e17fe827b45fbec0e35f
bluedevilx <sureshd@gmail.com>
2020-11-23 04:51:23 -0500

    inbound processing updates and misc update to handle schedule change email

--- app/ggt/tasks/outbound_lab_orders.py
+++ app/ggt/tasks/outbound_lab_orders.py
@@ -104,15 +104,19 @@ async def upload_insurance_files_from_gstore(orders):
         file_buffer = []
         for order in orders:
             if order['bill'] == 'Insurance Attached':
-                file_path_png = "{}/{}_001.png".format(local_insurance_card_file_path, order['id'])
-                filename = "{}_001.pdf".format(order['id'])
-                file_path_pdf = "{}/{}".format(local_insurance_card_file_path, filename)
-                appointment_id = order['id']
-                blob = await serve_file('ggt-insurance-cards-prod', '{}.png'.format(appointment_id))
-                if blob:
-                    blob.download_to_filename(file_path_png)
-                    Image.open(file_path_png).convert('RGB').save(file_path_pdf)
-                    file_buffer.append((filename, file_path_pdf))
+                try:
+                    file_path_png = "{}/{}_001.png".format(local_insurance_card_file_path, order['id'])
+                    filename = "{}_001.pdf".format(order['id'])
+                    file_path_pdf = "{}/{}".format(local_insurance_card_file_path, filename)
+                    appointment_id = order['id']
+                    blob = await serve_file('ggt-insurance-cards-prod', '{}.png'.format(appointment_id))
+                    if blob:
+                        blob.download_to_filename(file_path_png)
+                        Image.open(file_path_png).convert('RGB').save(file_path_pdf)
+                        file_buffer.append((filename, file_path_pdf))
+
+                except Exception as err:
+                    print(err)    
         
         print('uploading insurance files to FTP')
         upload_file_list_to_ftp(file_buffer)
@@ -238,7 +242,7 @@ def __get_formatted_row(order):
 
 def get_orders_ready_to_transmit():
     sql = """
-         SELECT 
+        SELECT 
             t.id AS id,
             t.patient_id AS patient_id,
             REPLACE(p.first_name, ',', '') AS first_name,
@@ -255,8 +259,7 @@ def get_orders_ready_to_transmit():
                 THEN
                     DATE_FORMAT(CONVERT_TZ(NOW(), '+00:00', '-06:00'),
                             '%m/%d/%y')
-                ELSE DATE_FORMAT(t.sample_collection_start_dt,
-                        '%m/%d/%y')
+                ELSE DATE_FORMAT(t.sample_collection_start_dt, '%m/%d/%y')
             END) AS date_of_collection,
             (CASE
                 WHEN (p.race = 'race_american_indian') THEN 'American Indian or Alaska Native'
@@ -293,15 +296,14 @@ def get_orders_ready_to_transmit():
             (CASE
                 WHEN
                     ((l.billing_type = 'insurance')
-                        AND (q.insurance_photo = 1))
+                        AND (q.has_insurance_photo = 1))
                 THEN
                     'Insurance Attached'
                 WHEN
                     ((l.billing_type = 'insurance')
-                        AND (LENGTH(q.insurance_photo) > 100))
+                        AND (q.has_insurance_photo <> 1))
                 THEN
-                    'Insurance Attached'
-                WHEN (l.billing_type = 'insurance') THEN 'Self-Pay'
+                    'Self-Pay'
                 ELSE 'Client Bill'
             END) AS bill,
             t.id AS client_order_number,
