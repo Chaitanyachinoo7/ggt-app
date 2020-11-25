a5a16bb878c4638c3adc95a9e1bac50f6357f0f5
bluedevilx <sureshd@gmail.com>
2020-11-24 19:21:35 -0500

    rollback temp fix

--- app/ggt/routers/rt_patient.py
+++ app/ggt/routers/rt_patient.py
@@ -131,8 +131,6 @@ async def api_verify_existing_patient(req: VerifyExistingPatientRequest):
 
 @router.get("/lookup_patient/{phone_number}")
 async def api_verify_existing_patient(phone_number: str):
-    #temp fix
-    return False
     return verify_existing_patient(
         phone_number,
         None
