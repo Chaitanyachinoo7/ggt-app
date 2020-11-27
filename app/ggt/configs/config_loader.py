008b0117db801192a3434fcec7a5d5d2cf151793
bluedevilx <sureshd@gmail.com>
2020-11-27 01:43:06 -0500

    redirected logging to cloud logger

--- app/ggt/configs/config_loader.py
+++ app/ggt/configs/config_loader.py
@@ -12,7 +12,6 @@ print('config_file', config_file)
 
 
 def __init_logging_config(path_to_file):
-    print('in __init_logging_config')
     with open(path_to_file, 'rt') as f:
         config_data = yaml.safe_load(f.read())
         print(config_data)
@@ -24,5 +23,5 @@ def __init_config(path_to_file):
         cfg = yaml.safe_load(f.read())
         return cfg
 
-__init_logging_config(log_config_file)
+#__init_logging_config(log_config_file)
 cfg = __init_config(config_file)
