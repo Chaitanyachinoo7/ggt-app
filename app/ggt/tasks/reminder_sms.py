cf28c64b6ee4046b4412de1c41992d61cc20de1a
Samar Swami <samarswami@Samars-MacBook-Air.local>
2020-11-28 20:26:21 -0500

    Daily appointment reminders emails will be sent now

--- app/ggt/tasks/reminder_sms.py
+++ app/ggt/tasks/reminder_sms.py
@@ -10,7 +10,7 @@ import itertools
 import shutil
 from pathlib import Path
 
-
+from ggt.lib.email import render_template
 from ggt.lib.utils import (
     get_config_val,
     log_generic,
@@ -96,10 +96,102 @@ def batch_enqueue_sms_notifications(data):
         print("err:", err)
 
 
+def task_process_daily_email_reminders():
+    try:
+        start = time.time()
+        print_header(
+            '\n\n******************Email Reminder Task started at [Start]******************************\n\n')
+        log_generic(
+            type="info",
+            function='task_process_email_reminders',
+            task_session_id=session_id,
+            info='Email Reminders started')
+        rows = get_appointments_for_today()
+        data = []
+        for row in rows:
+            email = formatted_email_message(row)
+            data.append(
+                (email['from_email'], email['from_name'],
+                 email['to_email'], email['subject'], email['html_content'])
+            )
+        data1 = list(chunks(data, 100))
+        for d in data1:
+            batch_enqueue_email_notifications(d)
+        log_generic(
+            type="info",
+            function='task_process_email_reminders',
+            task_session_id=session_id,
+            info='End Email Reminders')
+
+        print_header(
+            '\n\n****************** COMPLETED ******************************\nElapsed Time: {}\n'.format(time.time() - start))
+        return {
+            "success": True
+        }
+    except Exception as err:
+        print(err)
+
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
+def formatted_email_message(row):
+    try:
+        base_url = get_config_val('base_url')
+        from_email = get_config_val('notifications.from_email')
+        from_name = get_config_val('notifications.from_name')
+        subject = get_config_val('notifications.appointment_reminder_sublect')
+
+        template_vars = {
+            "first_name": row['first_name'],
+            "result_link": "{}/r/{}".format(base_url, row['token']),
+            "test_location_line1": str(row["addr1"]),
+            "test_location_line2": str(row["addr2"]),
+            "test_location_line3": str(row["city"]) + ", " + str(row["st"]) + " " + str(row["zip"]),
+            "test_date": str(row["scheduled_dt"].strftime('%I:%M%p')),
+            "test_number": str(row["id"]).rjust(6, '0'),
+            "appointment_link": "{}/appointment/{}/{}".format(get_config_val('base_url'), str(row["id"]).rjust(6, '0'), str(row["dob"]).replace('-', ''))
+        }
+        print(template_vars)
+        template_name = get_config_val(
+            'notifications.appointment_reminder_template')
+        html_content = render_template(template_name, **template_vars)
+
+        email_message = {
+            'from_email': from_email,
+            'from_name': from_name,
+            'to_email': row['email'],
+            'subject': subject,
+            'html_content': html_content
+        }
+
+        return email_message
+    except Exception as err:
+        print(err)
+
+
+def batch_enqueue_email_notifications(data):
+    try:
+        sql = """
+            INSERT INTO email_notification_queue_test_sam
+                (from_email, from_name, to_email, subject, html_content)
+            VALUES
+                (%s, %s, %s, %s, %s);
+        """
+        exec_batch_execute(sql, data)
+        return True
+
+    except Exception as err:
+        print("err:", err)
+        return False
+
+
 def get_appointments_for_today():
     try:
         sql = """
-        SELECT a.id, a.scheduled_dt, b.first_name, b.phone_number, b.dob, c.addr1, IFNULL(c.addr2,"") as addr2, c.city, c.st, c.zip 
+        SELECT a.id, a.scheduled_dt, b.first_name, b.phone_number, b.dob, b.token, b.email, c.addr1, IFNULL(c.addr2,"") as addr2, c.city, c.st, c.zip 
         FROM appointments a 
         JOIN patients b ON a.patient_id = b.id
         JOIN locations c ON a.location_id = c.id
