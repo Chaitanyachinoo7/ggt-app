a0b2c440a6e37173d9dcbd83f1159505d04b3b04
bluedevilx <sureshd@gmail.com>
2020-12-02 01:44:01 -0500

    async-fixes

--- app/ggt/models/workflow_models/admin_flow.py
+++ app/ggt/models/workflow_models/admin_flow.py
@@ -18,15 +18,15 @@ from ggt.models.process_models.bp_portal_experience import (
 ########################################################################################################
 
 
-def admin_get_all_test_results():
+async def admin_get_all_test_results():
     return y_response(
-        bp_get_all_test_results()
+        awaitbp_get_all_test_results()
     )
 
 
-def admin_generic_search(search_arr):
+async def admin_generic_search(search_arr):
     return y_response(
-        bp_get_general_search_results(search_arr)
+        await bp_get_general_search_results(search_arr)
     )
 
 ########################################################################################################
