fb73be3008a900858b389b6451a86674371cdc1a
bluedevilx <sureshd@gmail.com>
2020-12-08 15:05:23 -0500

    mysql adapater changes

--- app/ggt/lib/adapters/readonly_mysql_adapter.py
+++ app/ggt/lib/adapters/readonly_mysql_adapter.py
@@ -18,9 +18,10 @@ connection_config_dict = {
     'raise_on_warnings': True,
     'use_pure': False,
     'autocommit': True,
-    'pool_name': 'mypool',
+    'pool_name': 'readonlypool',
     'pool_size': 5
 }
+
 
 def __append_to_sql_log(log_type, sql_type, statement, details=""):
     return
@@ -64,17 +65,122 @@ def __append_to_sql_log(log_type, sql_type, statement, details=""):
             __cnx.close()
 
 
+def exec_insert(sql, val):
+    try:
+        __cnx = mysql.connector.connect(**connection_config_dict)
+        __cursor = __cnx.cursor(dictionary=True, buffered=True)
+
+        __cursor.execute(sql, val)
+        __cnx.commit()
+
+        __append_to_sql_log(
+            c.INFO, 'INSERT', __cursor.statement, __cursor.lastrowid)
+        return __cursor.lastrowid
+
+    except Error as err:
+        __append_to_sql_log(
+            c.ERROR,
+            'INSERT',
+            "{} / {}".format(sql, val),
+            err
+        )
+        return None
+
+    finally:
+        if (__cnx.is_connected()):
+            __cursor.close()
+            __cnx.close()
+
+
+def exec_batch_execute(sql, data):
+    try:
+        __cnx = mysql.connector.connect(**connection_config_dict)
+        __cursor = __cnx.cursor(dictionary=True, buffered=True)
+
+        __cursor.executemany(sql, data)
+        __cnx.commit()
+
+        return True
+
+    except Error as err:
+        __append_to_sql_log(
+            c.ERROR,
+            'EXECUTE MANY',
+            "{}".format(sql),
+            err
+        )
+        print(c.ERROR, 'EXECUTE MANY', "{}".format(sql), err)
+        return False
+
+    finally:
+        if (__cnx.is_connected()):
+            __cursor.close()
+            __cnx.close()
+
+
+def exec_update(sql, val=()):
+    try:
+        __cnx = mysql.connector.connect(**connection_config_dict)
+        __cursor = __cnx.cursor(dictionary=True, buffered=True)
+
+        __cursor.execute(sql, val)
+        __cnx.commit()
+        #__append_to_sql_log(c.INFO, 'UPDATE', __cursor.statement, __cursor.rowcount)
+        return True if __cursor.rowcount > 0 else False
+
+    except mysql.connector.Error as err:
+        __append_to_sql_log(
+            c.ERROR,
+            'UPDATE',
+            __cursor._executed,
+            err
+        )
+        return None
+
+    finally:
+        if (__cnx.is_connected()):
+            __cursor.close()
+            __cnx.close()
+
+
+def exec_delete(sql, val=()):
+    try:
+        __cnx = mysql.connector.connect(**connection_config_dict)
+        __cursor = __cnx.cursor(dictionary=True, buffered=True)
+
+        __cursor.execute(sql, val)
+        __cnx.commit()
+        __append_to_sql_log(
+            c.INFO,
+            'DELETE',
+            __cursor.statement,
+            __cursor.rowcount
+        )
+        return True if __cursor.rowcount > 0 else False
+
+    except mysql.connector.Error as err:
+        __append_to_sql_log(
+            c.ERROR,
+            'DELETE',
+            __cursor._executed,
+            err
+        )
+        return None
+
+    finally:
+        if (__cnx.is_connected()):
+            __cursor.close()
+            __cnx.close()
+
 
 def read_row(sql, vals):
+    log_generic(
+        type=c.INFO,
+        sql=sql,
+        vals=vals,
+        function=whoami()
+    )
     try:
-        '''
-        log_generic(
-            type=c.INFO,
-            sql=sql,
-            vals=vals,
-            function=whoami()
-        )
-        '''
         __cnx = mysql.connector.connect(**connection_config_dict)
         __cursor = __cnx.cursor(dictionary=True, buffered=True)
 
@@ -150,3 +256,29 @@ def read_rows(sql, vals=None):
         if (__cnx.is_connected()):
             __cursor.close()
             __cnx.close()
+
+
+def exec_sp(stored_procedure: str):
+    try:
+        __cnx = mysql.connector.connect(**connection_config_dict)
+        __cursor = __cnx.cursor(dictionary=True, buffered=True)
+
+        __cursor.callproc(stored_procedure)
+
+        return True
+
+    except mysql.connector.Error as err:
+        log_generic(
+            type=c.ERROR,
+            function=whoami(),
+            error=err,
+            executed=__cursor._executed
+        )
+
+    finally:
+        if (__cnx.is_connected()):
+            __cursor.close()
+            __cnx.close()
+
+    return False
+
