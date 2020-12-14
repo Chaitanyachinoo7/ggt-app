c8a056eb05890d2e0ce6a889d2013327a4cdc211
visithamanujaya <visitha@gogettested.com>
2020-12-14 21:47:10 +1100

    Created test cases that could be run using pyTest (GGT-271(

---
+++ app/tests/portal_test.py
@@ -0,0 +1,161 @@
+import os
+import sys
+import time
+
+import pytest
+import nest_asyncio
+
+nest_asyncio.apply()
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+from fastapi.testclient import TestClient
+from ggt.lib.db import read_rows, exec_delete
+from main import app
+from tests.resources import group, location
+
+client = TestClient(app)
+
+
+# def test_create_group():
+#     response = client.post(
+#         "/api/portal/site-admin/create_group",
+#         headers={"X-Token": "coneofsilence"},
+#         json=group
+#     )
+#
+#     r = response.json()
+#     assert response.status_code == 200
+#     assert r['status'] == 'success'
+#
+#
+# def test_create_location():
+#     response = client.post(
+#         "/api/portal/site-admin/create_location",
+#         headers={"X-Token": "coneofsilence"},
+#         json=location
+#     )
+#
+#     r = response.json()
+#     assert response.status_code == 200
+#     assert r['status'] == 'success'
+#
+#
+# @pytest.mark.asyncio
+# async def test_group_update():
+#     group_code = group['group_code']
+#     id = await get_group_id_by_code(group_code)
+#     _update = group
+#     _update['id'] = id
+#     _update['account'] = "TEST ACCOUNT 2"
+#     response = client.post(
+#         "/api/portal/site-admin/update_group",
+#         headers={"X-Token": "coneofsilence"},
+#         json=_update
+#     )
+#
+#     r = response.json()
+#     print(r)
+#     assert response.status_code == 200
+#     assert r['status'] == 'success'
+#     await delete_group_by_code(group_code)
+#
+#
+# async def get_group_id_by_code(code):
+#     sql = """SELECT * FROM groups WHERE group_code = %s"""
+#     vals = (code,)
+#     _group = await read_rows(sql, vals)
+#     return _group[0]['id']
+#
+#
+# async def delete_group_by_code(code):
+#     sql = """DELETE FROM groups WHERE group_code = %s"""
+#     vals = (code,)
+#     return await exec_delete(sql, vals)
+#
+
+''''
+Patient workflow
+'''
+
+
+def test_get_available_dates():
+    response = client.get(
+        "/api/get_available_dates/_DEFAULT_",
+        headers={"X-Token": "coneofsilence"}
+    )
+    r = response.json()
+    assert response.status_code == 200
+    assert r['status'] == 'success'
+
+
+def test_get_screen_flow_seq():
+    response = client.get(
+        "/api/get_screen_flow_seq/_DEFAULT_",
+        headers={"X-Token": "coneofsilence"}
+    )
+    r = response.json()
+    assert response.status_code == 200
+    assert r['status'] == 'success'
+
+
+def test_get_available_locations():
+    date = time.strftime('%Y-%m-%d')
+    response = client.get(
+        "/api/get_available_locations/_DEFAULT_/{}".format(date),
+        headers={"X-Token": "coneofsilence"}
+    )
+    r = response.json()
+    assert response.status_code == 200
+    assert r['status'] == 'success'
+
+
+def test_get_locations():
+    response = client.get(
+        "/api/get_locations",
+        headers={"X-Token": "coneofsilence"}
+    )
+    r = response.json()
+    assert response.status_code == 200
+    assert r['status'] == 'success'
+
+
+def test_get_locations_2():
+    response = client.get(
+        "/api/get_locations/_DEFAULT_",
+        headers={"X-Token": "coneofsilence"}
+    )
+    r = response.json()
+    assert response.status_code == 200
+    assert r['status'] == 'success'
+
+
+def test_get_locations_near_me_1():
+    response = client.get(
+        "/api/get_locations_near_me/32.779167/-96.808891",
+        headers={"X-Token": "coneofsilence"}
+    )
+    r = response.json()
+    assert response.status_code == 200
+    assert r['status'] == 'success'
+
+
+def test_get_locations_near_me_2():
+    response = client.get(
+        "/api/get_locations_near_me/32.779167/-96.808891/50",
+        headers={"X-Token": "coneofsilence"}
+    )
+    r = response.json()
+    assert response.status_code == 200
+    assert r['status'] == 'success'
+
+
+def test_get_locations_near_me_3():
+    response = client.get(
+        "/api/get_locations_near_me/_DEFAULT_/32.779167/-96.808891/50",
+        headers={"X-Token": "coneofsilence"}
+    )
+    r = response.json()
+    assert response.status_code == 200
+    assert r['status'] == 'success'
+
---
+++ app/tests/resources.py
@@ -0,0 +1,47 @@
+group = {
+    "account": "TEST ACCOUNT",
+    "group_code": "TEST_CODE",
+    "is_referral_code": 0,
+    "consent_req": 0,
+    "collect_insurance": 0,
+    "insurance_req": 0,
+    "allow_insurance_skip": 1,
+    "upfront_payment_req": 0,
+    "screen_seq": "is-patient,gender,race,ethnicity,symptoms,contact-tracing,public-places,"
+                  "patient-details,patient-address,patient-contact,patient-vitals,pre-existing-conditions,"
+                  "consent,date,location,time",
+    "required_screens": "",
+    "display_group_consent": 1,
+    "consent_party_name": "APIs for APIs",
+    "consent_url": "",
+    "logo_1": "",
+    "logo_2": "",
+    "optional_screens": ""
+}
+
+location = {
+    "name": "Site A",
+    "addr1": "123 any street",
+    "addr2": "suit one",
+    "addr3": "dd",
+    "city": "ds",
+    "st": "TX",
+    "zip": "121",
+    "time_zone": "CST",
+    "test_type_offered": "oral",
+    "status": "enabled",
+    "type": "drive_thru",
+    "billing_type": "insurance",
+    "collect_insurance_info": False,
+    "allow_insurance_skip": True,
+    "collect_upfront_payment": False,
+    "time_zone_offset": -240,
+    "group_ids": [
+        1,
+        2
+    ],
+    "service_ids": [
+        1
+    ],
+    "is_external": True
+}
