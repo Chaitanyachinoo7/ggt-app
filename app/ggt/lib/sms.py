2dcc38b43d0e30c4092f9d9e1c91d358347f1669
Samar Swami <samarswami@Samars-MacBook-Air.local>
2021-01-09 14:51:51 -0500

    Added short code integration

--- app/ggt/lib/sms.py
+++ app/ggt/lib/sms.py
@@ -3,12 +3,12 @@ from ggt.lib.adapters.sns_adapter import send_sns_sms
 from ggt.lib.adapters.pinpoint_adapter import send_pinpoint_message
 
 
-def send_sms(to_number, message, priority=2):
-    if priority == 1:
-        return send_twilio_sms_short_code(to_number, message)
+def send_sms(to_number, message, priority=1):
+    if priority == 2:
+        return send_twilio_sms(to_number, message)
     elif priority == 9:
         return send_pinpoint_message(to_number, message)
-    return send_twilio_sms(to_number, message)
+    return send_twilio_sms_short_code(to_number, message)
     # return send_sns_sms(to_number, message)
     # pass
 
