67fc14c36536157eee4f1a3347d733e26c5bbac9
visithamanujaya <visitha@gogettested.com>
2021-01-24 10:40:09 +1100

    Reverted back to SQS flow.

--- app/ggt/models/process_models/bp_schedules.py
+++ app/ggt/models/process_models/bp_schedules.py
@@ -264,7 +264,6 @@ def bp_ggv_get_schedule_locations_available_near_lat_lng(group_code, lat, lng, r
 
 
 def bp_get_schedule_times_available(location_id, date):
-    '''
     available_times = []
     try:
         request = {
@@ -287,19 +286,19 @@ def bp_get_schedule_times_available(location_id, date):
                 else:
                     if (datetime.now() - start_time).total_seconds() > 100:
                         return None
-        '''
-    rows = get_available_times(location_id, date)
-    available_times = []
-    try:
-        for row in rows:
-            d = datetime.strptime(str(row['start_time']), "%H:%M:%S")
 
-            available_times.append(
-                {
-                    "label": d.strftime("%I:%M %p"),
-                    "value": row['id']
-                }
-            )
+    # rows = get_available_times(location_id, date)
+    # available_times = []
+    # try:
+    #     for row in rows:
+    #         d = datetime.strptime(str(row['start_time']), "%H:%M:%S")
+    #
+    #         available_times.append(
+    #             {
+    #                 "label": d.strftime("%I:%M %p"),
+    #                 "value": row['id']
+    #             }
+    #         )
 
     except Exception as err:
         log_generic(
