da6730a859649680b56139a54128abcd588f807d
visithamanujaya <visitha@gogettested.com>
2021-01-24 00:46:58 +1100

    Match response structure Nicks request.

--- app/ggt/models/process_models/bp_schedules.py
+++ app/ggt/models/process_models/bp_schedules.py
@@ -330,10 +330,12 @@ def bp_get_second_shot_available_times(location_id, date):
                 res = read_from_dynamo(get_config_val('aws.dynamo_table_name'), msg_id)
                 if "Item" in res.keys():
                     temp = res['Item']
-                    for x in temp['available_dates']:
+                    for x in temp['available_times']:
                         for y in x['available_times']:
                             y['value'] = int(y['value'])
-                    return temp
+                    return {
+                        "available_dates": temp['available_times']
+                    }
                 else:
                     if (datetime.now() - start_time).total_seconds() > 100:
                         return None
