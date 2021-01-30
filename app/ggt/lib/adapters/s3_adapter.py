a8d0d441f82d650a63f6525e8b1e5f8c35c6dda1
bluedevilx <sureshd@gmail.com>
2021-01-30 15:04:08 -0500

    inbound reporting changes for direct online processing

--- app/ggt/lib/adapters/s3_adapter.py
+++ app/ggt/lib/adapters/s3_adapter.py
@@ -17,8 +17,10 @@ from ggt.lib.constants import (
 )
 
 
-default_link_expiration_time_limit = cfg('aws.default_link_expiration_time_limit')
+default_link_expiration_time_limit = cfg(
+    'aws.default_link_expiration_time_limit')
 lab_reports_bucket_name = cfg('aws.lab_reports_bucket_name')
+
 
 def __boto_connect_client(service):
     try:
@@ -37,7 +39,7 @@ def __boto_connect_client(service):
                 aws_secret_access_key=cfg('aws.secret_access_key'),
                 region_name=cfg('aws.region')
             )
-        
+
         return boto_client
 
     except Exception as err:
@@ -111,7 +113,6 @@ def create_folder(bucket_name, directory_name):
         return False
 
 
-
 def write_text_file(bucket, filename, body):
     try:
         if __boto_connect_client('s3').put_object(Body=body, Bucket=bucket, Key=filename):
@@ -123,24 +124,16 @@ def write_text_file(bucket, filename, body):
             function=whoami(),
             error=err
         )
-    
+
     return False
+    
 
-
-def move_file(source, destination, bucketName):
+def move_file(source_bucket, source_key, dest_bucket, dest_key):
     try:
-        print(source, destination)
-        copy = __boto_connect_client('s3').copy_object(
-            Bucket=bucketName, 
-            CopySource=source, 
-            Key=destination, 
-            MetadataDirective="COPY"
-        )
-        delete = __boto_connect_client('s3').delete_object(
-            Bucket=bucketName, 
-            Key=source.replace(bucketName+"/", "")
-        )
-        return True
+        if copy_file_from_s3_to_s3(source_bucket, source_key, dest_bucket, dest_key):
+            s3 = __boto_connect_resource('s3')
+            s3.Object(source_bucket, source_key).delete()
+            return True
 
     except Exception as err:
         log_generic(
@@ -148,7 +141,8 @@ def move_file(source, destination, bucketName):
             function=whoami(),
             error=err
         )
-        return False
+    
+    return False        
 
 
 def get_temp_lab_report_url(filename: str):
@@ -172,25 +166,75 @@ def get_temp_lab_report_url(filename: str):
         return None
 
 
-import boto3
+def get_list_of_files(bucket_name, prefix):
+    s3 = __boto_connect_resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    files = []
+
+    for file in bucket.objects.filter(
+        Prefix=prefix
+    ):
+        files.append(file.key)
+
+    return files
 
 
-def iterate_bucket_items(bucket):
+def get_file_iterator(bucket, prefix='', suffix=''):
     """
-    Generator that iterates over all objects in a given s3 bucket
+    Generate the keys in an S3 bucket.
 
-    See http://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Client.list_objects_v2 
-    for return data format
-    :param bucket: name of s3 bucket
-    :return: dict of metadata for an object
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetch keys that start with this prefix (optional).
+    :param suffix: Only fetch keys that end with this suffix (optional).
     """
+    s3 = boto3.client('s3')
+    kwargs = {'Bucket': bucket}
+
+    # If the prefix is a single string (not a tuple of strings), we can
+    # do the filtering directly in the S3 API.
+    if isinstance(prefix, str):
+        kwargs['Prefix'] = prefix
+
+    while True:
+
+        # The S3 API response is a large blob of metadata.
+        # 'Contents' contains information about the listed objects.
+        resp = s3.list_objects_v2(**kwargs)
+        for obj in resp['Contents']:
+            key = obj['Key']
+            if key.startswith(prefix) and key.endswith(suffix):
+                yield key
+
+        # The S3 API is paginated, returning up to 1000 keys at a time.
+        # Pass the continuation token into the next response, until we
+        # reach the final page (when this field is missing).
+        try:
+            kwargs['ContinuationToken'] = resp['NextContinuationToken']
+        except KeyError:
+            break
 
 
-    client = __boto_connect_client('s3')
-    paginator = client.get_paginator('list_objects_v2')
-    page_iterator = paginator.paginate(Bucket=bucket)
 
-    for page in page_iterator:
-        if page['KeyCount'] > 0:
-            for item in page['Contents']:
-                yield item
+def copy_file_from_s3_to_s3(source_bucket, source_key, dest_bucket, dest_key):
+    try:
+        s3 = __boto_connect_resource('s3')
+
+        copy_source = {
+            'Bucket': source_bucket,
+            'Key': source_key
+        }
+
+        bucket = s3.Bucket(dest_bucket)
+        bucket.copy(copy_source, dest_key)
+        return True
+
+    except Exception as err:
+        log_generic(
+            type=ERROR,
+            function=whoami(),
+            operation='{}/{} ==> {}/{}'.format(source_bucket, source_key, dest_bucket, dest_key),
+            error=err
+        )
+    
+    return False
+
--- app/ggt/tasks/misc_processor.py
+++ app/ggt/tasks/misc_processor.py
@@ -165,15 +165,15 @@ def process_email_notifications():
 def formatted_email_message(row):
     from_email = cfg('notifications.from_email')
     from_name = cfg('notifications.from_name')
-    #subject = "IMPORTANT: {}, Important information regarding Covid-19 Testing".format(row['first_name'])
-    subject = "IMPORTANT: {}, Your Covid-19 Testing location has closed due to inclement weather".format(row['first_name'])
+    subject = "IMPORTANT: {}, Important information regarding Covid-19 Testing".format(row['first_name'])
+    #subject = "IMPORTANT: {}, Your Covid-19 Testing location has closed due to inclement weather".format(row['first_name'])
 
     template_vars = {
         "first_name": row['first_name']
     }
 
-    template_name = 'GGT-14-APPOINTMENT-WEATHER-CLOSING-EMAIL.html'
-    #template_name = 'GGT-9-APPOINTMENT-DELAYED-EMAIL.html'
+    #template_name = 'GGT-14-APPOINTMENT-WEATHER-CLOSING-EMAIL.html'
+    template_name = 'GGT-9-APPOINTMENT-DELAYED-EMAIL.html'
     html_content = render_template(template_name, **template_vars)
 
     email_message = {
@@ -405,10 +405,10 @@ def get_appointments():
             patients p ON a.patient_id = p.id
         WHERE
             location_id IN (
-                2420,370,2422,2385,276,373,367,399,2414,2446,2447,2415,2423,2417,2448,2416,274,369, 372, 2421
+                2420,370,2422,2424,2385,367,399,2485,361,2414,2446,2419,2447,2415,2423,371,2417,2416,366, 692, 419
             )
-            AND scheduled_dt > '2021-01-27 00:00:00'
-            AND scheduled_dt < '2021-01-28 00:00:00'
+            AND scheduled_dt > '2021-01-30 00:00:00'
+            AND scheduled_dt < '2021-01-31 00:00:00'
             AND status = 'scheduled'
         """
 
@@ -422,7 +422,7 @@ def prepare_sms_text(appointment):
     #return """Hi {}, the testing location where you have registered will be closed on January 18th, 20th, and 21st. We apologize for the inconvenience this may cause. Please visit GoGetTested.com/Kansas to register for a new appointment.
     #""".format(appointment["first_name"])
 
-    return """Hi {}, due to inclement weather, the location where you have registered for your COVID-19 test will be CLOSED. We apologize for the inconvenience this may cause. Please visit GoGetTested.com to register for a new appointment.
+    return """Hi {}, due to inclement weather the location where you have reigstered for your COVID-19 test will have a delayed start until 2pm. We apologize for the inconvenience. Please visit GoGetTested.com to register for a new appointment.
     """.format(appointment["first_name"])
 
 
