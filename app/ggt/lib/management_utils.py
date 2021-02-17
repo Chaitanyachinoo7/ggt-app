c3d42d9c583aa9110a5e8d54b302eb3ec56d6514
visithamanujaya <visitha@gogettested.com>
2021-02-17 23:16:47 +1100

    Added confirmation email for organization creation request (GGT-460)

--- app/ggt/lib/management_utils.py
+++ app/ggt/lib/management_utils.py
@@ -3,6 +3,18 @@ from ggt.lib.adapters.sendgrid_adapter import send_email
 from ggt.lib.email import render_template
 
 
+def send_new_account_registration_request_email(first_name, email):
+    from_email = cfg('notifications.from_email')
+    from_name = cfg('notifications.from_name')
+    subject = "GoGetVax provider account creation request received"
+    template_vars = {
+        "first_name": first_name
+    }
+    template_name = 'GGV-2-NEW_ACCOUNT_REQUEST-EMAIL.html'
+    html_content = render_template(template_name, **template_vars)
+    send_email(from_email, from_name, email, subject, html_content)
+
+
 def send_new_account_creation_email(first_name, email, password):
     from_email = cfg('notifications.from_email')
     from_name = cfg('notifications.from_name')
