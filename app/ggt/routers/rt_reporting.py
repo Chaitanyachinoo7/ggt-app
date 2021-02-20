c64788e91749477a295ef50f4ab2bdcb655207b9
visithamanujaya <visitha@gogettested.com>
2021-02-21 00:43:50 +1100

    Added new dashboard API (GGT-467)

--- app/ggt/routers/rt_reporting.py
+++ app/ggt/routers/rt_reporting.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Security
 
 from ggt.lib.auth import authorize_user
 from ggt.models.workflow_models.reporting_flow import get_stats_today, get_stats_by_date, get_sms_stats_by_date, \
-    get_email_stats_by_date, aging_samples_with_lab, get_user_activity, get_patient_drill_down_by_date
+    get_email_stats_by_date, aging_samples_with_lab, get_user_activity, get_patient_drill_down_by_date, \
+    get_portal_stats_today
 from ggt.models.data_models.data_types import PermissionsEnum as p, SummaryByDate, UserActivity, PatientDrilldownRequest
 
 router = APIRouter()
@@ -11,6 +12,11 @@ router = APIRouter()
 @router.post("/get_stats_today", dependencies=[Security(authorize_user, scopes=[p.ANONYMOUS])])
 async def api_get_stats_today():
     return get_stats_today()
+
+
+@router.get("/get_portal_stats_today")
+async def api_get_portal_stats_today(user=Security(authorize_user, scopes=[p.CREATE_LOCATION])):
+    return get_portal_stats_today(user)
 
 
 @router.post("/get_stats_by_date")
