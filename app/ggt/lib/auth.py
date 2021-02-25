79c5ab48bf683e5ca5a0d377652f2c1475e55dde
visithamanujaya <visitha@gogettested.com>
2021-02-25 22:30:12 +1100

    Commented out the JWT bypass.

--- app/ggt/lib/auth.py
+++ app/ggt/lib/auth.py
@@ -117,8 +117,8 @@ def get_rsa_key_auth0(token):
 
 
 def authorize_user(security_scopes: SecurityScopes, token: str = Depends(oauth2_scheme)):
-    if (get_config_val('env') == 'LOCAL-PROD' or get_config_val('env') == 'DEV'):  # Allow auth override for local-prod
-        return True
+    # if (get_config_val('env') == 'LOCAL-PROD' or get_config_val('env') == 'DEV'):  # Allow auth override for local-prod
+    #     return True
     try:
         scopes = security_scopes.scopes
         if p.ANONYMOUS in scopes:
