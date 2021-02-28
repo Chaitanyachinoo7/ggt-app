fb8afdee1a8ba849ce9ee59fc300e430871d496b
Theekshana Dilshan <theekshana@gogettested.com>
2021-02-28 13:01:02 +0530

    [GGT-525] Allow various language support in Stripe - Fix invalid KeyError

--- app/ggt/lib/utils.py
+++ app/ggt/lib/utils.py
@@ -341,5 +341,5 @@ def get_translated_message(message_id = str):
     # Define inner function to get the message in specified language such as en, es, ...
     def get_language(lang):
         # If specified language is not there, return the default
-        return message_in_langs[lang] if message_in_langs[lang] else message_in_langs['default']
+        return message_in_langs[lang] if lang in message_in_langs else message_in_langs['default']
     return get_language  # Return the inner function so that caller can do get_translated_message('id')('es')
