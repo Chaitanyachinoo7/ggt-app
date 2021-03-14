3b21eb506142cb3b95871a24ae8690b8b944be5f
Sourav Johar <johar.sourav97@gmail.com>
2021-03-14 13:18:20 +0530

    added lab integration tests (GGT-551)

--- app/ggt/lib/adapters/s3_adapter.py
+++ app/ggt/lib/adapters/s3_adapter.py
@@ -93,7 +93,7 @@ def create_folder(bucket_name, directory_name):
     try:
         response = __boto_connect_client('s3').put_object(
             Bucket=bucket_name,
-            Key=(directory_name+'/')
+            Key=(directory_name + '/')
         )
         log_generic(
             type=INFO,
@@ -217,6 +217,8 @@ def get_file_iterator(bucket, prefix='', suffix='', get_last_modified=False):
         # The S3 API response is a large blob of metadata.
         # 'Contents' contains information about the listed objects.
         resp = s3.list_objects_v2(**kwargs)
+        if resp['KeyCount'] == 0:
+            return []
         for obj in resp['Contents']:
             key = obj['Key']
             last_modified = obj['LastModified']
@@ -233,7 +235,6 @@ def get_file_iterator(bucket, prefix='', suffix='', get_last_modified=False):
             kwargs['ContinuationToken'] = resp['NextContinuationToken']
         except KeyError:
             break
-
 
 
 def copy_file_from_s3_to_s3(source_bucket, source_key, dest_bucket, dest_key):
@@ -253,7 +254,8 @@ def copy_file_from_s3_to_s3(source_bucket, source_key, dest_bucket, dest_key):
         log_generic(
             type=ERROR,
             function=whoami(),
-            operation='{}/{} ==> {}/{}'.format(source_bucket, source_key, dest_bucket, dest_key),
+            operation='{}/{} ==> {}/{}'.format(source_bucket,
+                                               source_key, dest_bucket, dest_key),
             error=err
         )
 
@@ -287,7 +289,7 @@ def uploadDirectory(path, bucketName):
         for root, dirs, files in os.walk(path):
             for file in files:
                 __boto_connect_client('s3').upload_file(os.path.join(
-                    root, file), bucketName, "brownwoodv/"+path+'/'+file)
+                    root, file), bucketName, "brownwoodv/" + path + '/' + file)
     except Exception as err:
         log_generic(
             type=ERROR,
@@ -296,9 +298,10 @@ def uploadDirectory(path, bucketName):
         )
         return None
 
+
 def get_temp_vaccine_consent_url(filename: str, lab_reports_bucket_name=lab_reports_bucket_name):
     try:
-        url = __boto_connect_client('s3','us-east-1').generate_presigned_url(
+        url = __boto_connect_client('s3', 'us-east-1').generate_presigned_url(
             ClientMethod='get_object',
             Params={
                 'Bucket': lab_reports_bucket_name,
