7652c7dd86f2be32a3a38e0ac1b414e73c873a10
Theekshana Dilshan <theekshana@gogettested.com>
2021-03-24 20:36:27 +0530

    [GGT-585] - API to extract daily portal stat by location - Exposing new end-point - This API will be similar to portal stat today but divided by location - no show and hourly information are integrated

--- app/ggt/models/process_models/bp_reporting.py
+++ app/ggt/models/process_models/bp_reporting.py
@@ -5,7 +5,7 @@ from cachetools import cached, LRUCache, TTLCache
 
 from ggt.models.data_models.reporting import get_stats_today, get_stats_by_date, get_sms_stats_by_date, \
     get_email_stats_by_date, aging_samples_with_lab_by_ship_date, get_user_activity, get_patient_drill_down_by_date, \
-    get_portal_stats_today
+    get_portal_stats_today, get_portal_stats
 
 
 def bp_get_stats_today():
@@ -40,3 +40,8 @@ def bp_get_user_activity(req):
 
 def bp_get_patient_drill_down_by_date(location_id, date, status, org_id):
     return get_patient_drill_down_by_date(location_id, date, status, org_id)
+
+
+@cached(cache=TTLCache(maxsize=1024, ttl=60))
+def bp_get_portal_stats(org_id, location_id, timestamp):
+    return get_portal_stats(org_id, location_id, timestamp)
