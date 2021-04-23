e2d3c8d46c0acf99cbb0f187c30bec2b8549c279
Samar Swami <samarswami@Samars-MacBook-Air.local>
2021-04-23 00:15:16 -0400

    Added extra logs to pkpass generation flow

--- app/ggt/models/process_models/bp_patient_experience.py
+++ app/ggt/models/process_models/bp_patient_experience.py
@@ -1181,10 +1181,10 @@ def __generate_pk_pass(pkpass_req, patient, verification):
             for filename in files:
                 print(filename)
         passfile.addFile("icon.png", open(
-            os.environ['LAMBDA_TASK_ROOT'] + "/ggt/configs/images/Group 4GGV-4.png", "rb"))
+            "./app/ggt/configs/images/Group 4GGV-4.png", "rb"))
         print("ggt/configs/images/Group 4GGV-4.png was found")
         passfile.addFile("logo.png", open(
-            os.environ['LAMBDA_TASK_ROOT'] + "/ggt/configs/images/Group 4GGV-4.png", "rb"))
+            "./app/ggt/configs/images/Group 4GGV-4.png", "rb"))
         print("/ggt/configs/images/Group 4GGV-4.png was found")
         print("pkpass temp path:","/tmp/{}.{}".format(str(patient["patient_id"]), "pkpass"))
         _ = passfile.create(cert_pem,
