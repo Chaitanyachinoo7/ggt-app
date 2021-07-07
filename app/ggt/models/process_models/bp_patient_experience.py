1a4b6f442074506bfae629c263ca91e29fb64692
Samar Swami <samarswami@Samars-Air.lan>
2021-07-07 10:02:39 -0400

    Android pass update changes

--- app/ggt/models/process_models/bp_patient_experience.py
+++ app/ggt/models/process_models/bp_patient_experience.py
@@ -1589,7 +1589,7 @@ def __makeOauthCredential():
     try:
         # the variables are in config file
         credentials = service_account.Credentials.from_service_account_file(
-            './app/ggt/configs/gpay/ggt-pfe-prod-e3201b1cc798.json',
+            './ggt/configs/gpay/ggt-pfe-prod-e3201b1cc798.json',
             scopes=['https://www.googleapis.com/auth/wallet_object.issuer'])
 
         return credentials
