85929c99a7d4cef26e05bd6335e1faf2259bd3a8
visithamanujaya <visitha@gogettested.com>
2021-08-20 12:23:18 +0530

    Added error handling.

--- app/ggt/models/process_models/bp_patient_experience.py
+++ app/ggt/models/process_models/bp_patient_experience.py
@@ -1013,8 +1013,8 @@ def __sanitize_names(req: LookupGGVAddVaxCertRequest):
     req.last_name = req.last_name.strip()
 
     if req.pristine:
-        req.pristine.first_name = req.pristine.first_name.strip()
-        req.pristine.last_name = req.pristine.last_name.strip()
+        req.pristine.first_name = req.pristine.first_name.strip() if req.pristine.first_name else None
+        req.pristine.last_name = req.pristine.last_name.strip() if req.pristine.last_name else None
     return req
 
 
