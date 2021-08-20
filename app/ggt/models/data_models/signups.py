371844cc26af500bee363f21b30f2ddc8a2d62cc
Samar Swami <samarswami@Samars-MacBook-Air.local>
2021-08-19 21:43:57 -0400

    OTP fixes

--- app/ggt/models/data_models/signups.py
+++ app/ggt/models/data_models/signups.py
@@ -94,8 +94,8 @@ def get_signup_record_by_phone_otp(phone_number, otp):
             signups 
         WHERE 
             phone_number = %s 
-            AND otp = 
-            AND create_dt > DATE_SUB(NOW(), INTERVAL 5 MINUTE)%s
+            AND otp = %s
+            AND create_dt > DATE_SUB(NOW(), INTERVAL 5 MINUTE)
         """
         vals = (phone_number, otp)
         row = replica_read_row(sql, vals)
