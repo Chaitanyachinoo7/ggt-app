f874c5c57cc3d63ed71978f8ba3740046c3fa322
Samar Swami <samarswami@Samars-MacBook-Air.local>
2021-09-23 14:43:45 -0400

    Added pkpass update call on portal verification flow

--- app/ggt/models/process_models/bp_patient_experience.py
+++ app/ggt/models/process_models/bp_patient_experience.py
@@ -916,7 +916,8 @@ def bp_send_wallet_pass_update_to_apple(token):
     try:
         cert = (cert_pem, ssl_key, key_pem_password)
         client = httpx.Client(http2=True, cert=cert)
-        r = client.post('https://api.push.apple.com/3/device/'+token, headers={'apns-push-type': 'alert'},
+        r = client.post('https://api.push.apple.com/3/device/'+token, headers={'apns-push-type': 'alert', 'LastModified': datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S")},
                         data={"aps": {"alert": "GoGetDoc Pass Update"}})
         print(r)
         return True
--- app/ggt/routers/rt_patient.py
+++ app/ggt/routers/rt_patient.py
@@ -350,7 +350,7 @@ def api_vax_wallet_pass_apple_upadte_serial(device_id: str = None, pass_type: st
     return vax_wallet_pass_apple_upadte_serial(device_id, Authorization)
 
 
-@router.delete("/vax_wallet_pass_apple_upadte/v1/devices/{device_id}/registrations/{pass_type}")
+@router.delete("/vax_wallet_pass_apple_upadte/v1/devices/{device_id}/registrations/{pass_type}/{serial_no}")
 def api_vax_wallet_pass_apple_delete(device_id: str = None, pass_type: str = None, Authorization: str = Header(None)):
     return True
 
