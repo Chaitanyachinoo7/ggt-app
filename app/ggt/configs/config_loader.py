7e5e82cb11aca6bbe0c80ab51f4e44e582b26e11
Chaitanyachinoo7 <Chaitanyachinoo7@users.noreply.github.com>
2026-05-09 11:04:16 +0000

    feat: Generate Project Code Wiki Documentation
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- app/ggt/configs/config_loader.py
+++ app/ggt/configs/config_loader.py
@@ -1,22 +1,17 @@
 import yaml
-#import logging
 from logging import config
 from pathlib import Path
 
 from ggt.lib.adapters.secret_adaptor import get_secret_value
 
 curr_file = Path(__file__)
-print('curr_file:', curr_file)
 log_config_file = curr_file.parent.joinpath("log-config.yml")
-print('log_config_file:', log_config_file)
 config_file = curr_file.parent.joinpath("config.yml")
-print('config_file', config_file)
 
 
 def __init_logging_config(path_to_file):
     with open(path_to_file, 'rt') as f:
         config_data = yaml.safe_load(f.read())
-        print(config_data)
         config.dictConfig(config_data)
 
 
@@ -25,8 +20,6 @@ def __init_config(path_to_file):
         envs = yaml.safe_load(f.read())
         stage = envs['env']
         secret_name = envs['secret_name']
-        print('*******************ENVIRONMENT***************** - {}'.format(stage))
-        print('*******************SECRET NAME***************** - {}'.format(secret_name))
         if stage == 'LOCAL':
             return envs
         else:
